93c4f6fc53893650da29d0c9a323a4a23c6b118c
Aleksandr Desyatnichenko <alik_kenig@rambler.ru>
2022-12-01 22:22:29 +0200

    Search done

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,56 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+
+    private Map<String, List<PageEntry>> wordsMap;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+
+        wordsMap = new HashMap<>();
+
+        for (File file : pdfsDir.listFiles()) {
+            PdfDocument doc = new PdfDocument(new PdfReader(file));
+            for (int pageNum = 1; pageNum <= doc.getNumberOfPages(); pageNum++) {
+                PdfPage page = doc.getPage(pageNum);
+                String text = PdfTextExtractor.getTextFromPage(page);
+                String[] words = text.split("\\P{IsAlphabetic}+");
+
+                Map<String, Integer> wordsFreq = new HashMap<>();
+
+                for (String word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    wordsFreq.put(word, wordsFreq.getOrDefault(word, 0) + 1);
+                }
+
+                for (String word : wordsFreq.keySet()) {
+                    PageEntry pageEntry = new PageEntry(file.getName(), pageNum, wordsFreq.get(word));
+                    if (!wordsMap.containsKey(word)) {
+                        List<PageEntry> pageEntryList = new ArrayList<>();
+                        pageEntryList.add(pageEntry);
+                        wordsMap.put(word, pageEntryList);
+                    } else {
+                        wordsMap.get(word).add(pageEntry);
+                    }
+                }
+            }
+        }
     }
+
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        List<PageEntry> result = wordsMap.get(word);
+        Collections.sort(result);
+        return result;
     }
 }
